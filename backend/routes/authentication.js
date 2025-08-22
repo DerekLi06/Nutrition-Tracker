69fde7f862757364398ae34fee0c7539c2f8ebb0
Derek Li <derekli0987@gmail.com>
2025-08-22 00:25:26 -0400

    added registeration + login authentication and password hashing

---
+++ backend/routes/authentication.js
@@ -0,0 +1,35 @@
+const {Router} = require("express");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const authenticationRouter = Router();
+const UserM = require("../model/UserM");
+
+authenticationRouter.post("/register", async (req, res) => {
+    const data = req.body;
+    const curUser = await UserM.findOne({email: data.email});
+    if (curUser) {return res.status(400).send({message: "This User Already Exists!"});}
+    bcrypt.hash(data.password, 8, async function(err, hash) {
+        if (err) {return res.status(400).send({message: "Error Message 400: A Problem Has Occured"});}
+        const newU = new UserM({...data, password: hash});
+        await newU.save()
+        return res.status(200).send({message: "New User Registered!"});
+    });
+});
+
+authenticationRouter.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    const curUser = await UserM.findONe({email: data.email});
+    if (!curUser) {return res.status(400).send({message: "This User Doesn't Exist!"})}
+    bycrpt.compare(password, curUser.password, function(err, valid) {
+        if (err) {return res.status(400).send({message: "Error Message 400: A Problem Has Occured"});}
+        if (!valid) {return res.status(400).send({message: "Invalid Username or Password! Try Again!"});}
+        const token = jwt.sign({
+            userId: curUser._id
+        }, process.env.JWT_SECRET, {expiresIn: "24h"});
+        return res.status(200).send({message: "User Successfully Logged In!", token});
+    });
+});
+
+
+    
