f2f6c316c0bea23e5592f07c419690e8821b0d8d
Derek Li <derekli0987@gmail.com>
2025-08-22 21:57:51 -0400

    routes for creating and retrieving food items

---
+++ backend/routes/food.js
@@ -0,0 +1,22 @@
+const {Router} = require("express");
+const foodsRouter = Router();
+
+const FoodM = require("../models/FoodM");
+
+foodsRouter.post("/create", async (request, result) => {
+    try {
+        const data = request.body;
+        await FoodM.insertMany(data);
+        return result.status(200).send({message: "Food Items Save to Database!"});
+    } catch (err) {
+        return result.status(400).send({message: err.message || "Failed to Save Food Items!"})
+    }
+});
+
+foodsRouter.get("/", async (request, result) => {
+    const query = request.query;
+    const foods = await FoodM.find({"name": { $regex: query, $options: "i"}});
+    result.status(200).send(foods)
+});
+
+module.exports = foodsRouter;
